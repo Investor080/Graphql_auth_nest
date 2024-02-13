9dc8b57181e588430ad40e13b0e7dac1b9e4d6ea
unknown <alamoyoolayinka@gmail.com>
2024-02-13 21:00:23 +0000

    completed

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+
 // import { CorsModule } from '@nestjs/common';
 import * as csurf from 'csurf';
-
+import * as helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  app.use(csurf());
+  app.enableCors();
+  app.use(express.urlencoded({ extended: false }));
+  app.use(csurf({ cookie: true }));
+  // app.use(helmet());
+
+  // const graphqlOptions = new GraphQLModule({
+  //   autoSchemaFile: 'schema.gql',
+  //   context: ({ req }) => ({ req }),
+  // });
+  // graphqlOptions.applyMiddleware({ app });
+
 app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
