22231e1015fe050c544627e651c8ee685a9c8c16
unknown <alamoyoolayinka@gmail.com>
2024-02-13 19:21:59 +0000

    done

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import { CorsModule } from '@nestjs/common';
+import * as csurf from 'csurf';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  // app.enableCors();
+  app.use(csurf());
+app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
