27053d7ed3a9f93c8e8ab4f37a057d8092b37e58
unknown <alamoyoolayinka@gmail.com>
2024-02-14 00:49:41 +0000

    completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
 import * as csurf from 'csurf';
 import * as cookieParser from 'cookie-parser';
 
