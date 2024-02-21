87b1eefc066c19387f1d65a731716719cadd4de3
unknown <alamoyoolayinka@gmail.com>
2024-02-21 12:38:12 +0000

    completed

--- src/partner/partner.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PartnerResolver } from './partner.resolver';
-
-@Module({
-  providers: [PartnerResolver]
-})
-export class PartnerModule {}
--- src/student/student.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StudentResolver } from './student.resolver';
-
-@Module({
-  providers: [StudentResolver]
-})
-export class StudentModule {}
